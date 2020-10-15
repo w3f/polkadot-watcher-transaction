4bbeae064253acab684c91a717bfecf3e1df7112
ironoa <alessio.onori@gmail.com>
2020-10-15 14:16:51 +0200

    added default prometheus metrics

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import {  } from "express/";
 import { createLogger } from '@w3f/logger';
 import { Config } from '@w3f/config';
-
+import { Prometheus } from '../prometheus';
 import { Subscriber } from '../subscriber';
 import { Matrixbot } from '../matrixbot';
 import { InputConfig } from '../types';
 
-const _startServer = (port: number): void =>{
+const _startServer = (port: number): express.Application =>{
   const server = express();
 
   server.get('/healthcheck',
@@ -15,15 +16,22 @@ const _startServer = (port: number): void =>{
       })
       
   server.listen(port);
+  return server
 }
 
 export const startAction = async (cmd): Promise<void> =>{
     const cfg = new Config<InputConfig>().parse(cmd.config);
+    
+    const logger = createLogger(cfg.logLevel);
+    
+    const server = _startServer(cfg.port)
 
-    _startServer(cfg.port)
+    const promClient = new Prometheus(logger);
+    promClient.injectMetricsRoute(server)
+    promClient.startCollection()
 
     const notifier = new Matrixbot(cfg.matrixbot.endpoint);
-    const logger = createLogger(cfg.logLevel);
+
     const subscriber = new Subscriber(cfg,notifier,logger);
     await subscriber.start();
 }
