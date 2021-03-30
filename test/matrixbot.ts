309057bdd1019c18ffa1db6c311c5b65525d9030
ironoa <alessio.onori@gmail.com>
2021-03-30 02:15:40 +0200

    last changes

--- test/matrixbot.ts
+++ test/matrixbot.ts
@@ -27,12 +27,6 @@ const getExpectedBalanceChangeMessage = (name: string,address: string): string =
 const expectedBalanceDecreasedAlertname = 'BalanceDecreased';
 const expectedBalanceIncreasedAlertname = 'BalanceIncreased';
 
-// const balanceTransfer = "0x00000000000000000000045c6c458e00" as unknown as Balance
-// const expectedTransactionEventSentMessage = `New Transfer of 56.7210 KSM sent from the account ${senderName}, check https://${networkId}.subscan.io/account/${senderAddress}?tab=transfer for details.`;
-// const expectedTransactionEventSentAlertname = 'TransactionSent';
-// const expectedTransactionEventReceivedMessage = `New Transfer of 56.7210 KSM received in the account ${receiverName}, check https://${networkId}.subscan.io/account/${receiverAddress}?tab=transfer for details.`;
-// const expectedTransactionEventReceivedAlertname = 'TransactionReceived';
-
 const mockRestNotifier = (alertname: string, message: string): void => {
   nock(host).post(`/${path}`,_.matches(
     {
@@ -52,20 +46,6 @@ describe('Matrixbot', () => {
         afterEach(() => {
             nock.cleanAll();
         });
-
-        // it('notifies Event transaction sent detected', async () => {
-        //   mockRestNotifier(expectedTransactionEventSentAlertname,expectedTransactionEventSentMessage)
-
-        //   const data = {
-        //       name: senderName,
-        //       txType: TransactionType.Sent,
-        //       address: senderAddress,
-        //       networkId: networkId,
-        //       amount: balanceTransfer
-        //   };
-
-        //   await subject.newBalanceChange(data);
-        // });
 
         it('notifies balance decreased', async () => {
           mockRestNotifier(expectedBalanceDecreasedAlertname,getExpectedBalanceChangeMessage(senderName,senderAddress))
