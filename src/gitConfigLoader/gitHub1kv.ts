e721f6435646d477c0373a24e38b2422115b9814
Alessio Onori <alessio.onori@gmail.com>
2023-05-27 20:39:54 +0200

    Update gitHub1kv.ts

--- src/gitConfigLoader/gitHub1kv.ts
+++ src/gitConfigLoader/gitHub1kv.ts
@@ -2,7 +2,6 @@ import { GitConfigLoader } from "./gitConfigLoaderInterface";
 import fetch from 'node-fetch';
 import { Subscribable } from "../types";
 import { TargetFromGit1kv } from "./types";
-import { parse } from 'yaml'
 
 export class GitHub1kv implements GitConfigLoader {
 
@@ -12,10 +11,9 @@ export class GitHub1kv implements GitConfigLoader {
 
   async downloadAndLoad(): Promise<Array<Subscribable>> {
     const response = await fetch(this.url);
-    let data = await response.text();
+    const data = await response.json();
     // based on the shape of https://github.com/w3f/1k-validators-be/blob/master/helmfile.d/config/kusama/otv-backend-prod.yaml.gotmpl
-    data = data.replace(/{{.*}}/gm, '')
-    const candidates: Array<TargetFromGit1kv> = JSON.parse(parse(data)["config"])["scorekeeper"]["candidates"]
+    const candidates: Array<TargetFromGit1kv> = data.candidates
 
     return candidates.map(c=>{
       const target: Subscribable = {
